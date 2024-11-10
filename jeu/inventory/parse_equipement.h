0db8c1d3285a94251b42864015cd62d4d260b040
thyma <thyma@exemple.com>
2024-11-11 00:05:02 +0200

    factorization with insert function in parse_equipement

--- jeu/inventory/parse_equipement.h
+++ jeu/inventory/parse_equipement.h
@@ -8,4 +8,5 @@ void actualise_stat(struct personnages *p);
 void actualise_stat_building(struct building *b);
 void init_stat(struct personnages *p);
 void init_stat_building(struct building *b);
+//void insert(float x, float y, int id, char floor, int offsetx, int offsety, struct personnages *p, struct building *b, SDL_Texture* img);
 #endif /*STAT*/
